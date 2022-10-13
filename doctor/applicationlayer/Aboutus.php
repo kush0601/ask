0ef5f3a5b945ba283667484ec102037a1c649b3c
kush0601 <110459846+kush0601@users.noreply.github.com>
2022-10-13 11:00:59 +0530

    Add files via upload

---
+++ doctor/applicationlayer/Aboutus.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> About us page </title>
+        <link rel="stylesheet"  href="Aboutus.css">
+        <link href="https://fonts.googleapis.com/css2?family=Alfa+Slab+One&family=Open+Sans:wght@300&display=swap" rel="stylesheet">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width">
+	<meta http-equiv="X-UA-Compatible" content="id=edge">
+</head>
+
+<body>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
